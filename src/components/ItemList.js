2b147130a7d56c3a2ff0adff2dbfb07226e87bc2
ericbanker12 <ericbanker12@gmail.com>
2019-09-23 14:35:53 -0500

    add ItemList component

---
+++ src/components/ItemList.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+//import {  } from 'reactstrap'
+import axios from 'axios'
+
+//import Item from './Item'
+
+function ItemList(props) {
+    const [items, setItems] = useState([])
+
+    return (
+        <div>
+            {/* {items.map(item => <Item {...item} />)} */}
+        </div>
+    )
+}
