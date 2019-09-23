530c55f02a094965d6c45ef73425682bd46f460c
ericbanker12 <ericbanker12@gmail.com>
2019-09-23 14:44:52 -0500

    add styling

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-//import {  } from 'reactstrap'
+import React, { useState, useEffect } from 'react'
+import { Table } from 'reactstrap'
 import axios from 'axios'
 
 //import Item from './Item'
@@ -8,8 +8,13 @@ function ItemList(props) {
     const [items, setItems] = useState([])
 
     return (
-        <div>
-            {/* {items.map(item => <Item {...item} />)} */}
-        </div>
+        <Table>
+            <thead>
+                <h3>My Top Nine <span className="text-muted">{props.category}</span></h3>
+            </thead>
+            <tbody>
+                {/* {items.map(item => <Item {...item} />)} */}
+            </tbody>
+        </Table>
     )
 }
