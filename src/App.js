7ee08d24487c8cdefc2779d0fe82d416227c7d4c
ian-sanchez <silkyhairbear@gmail.com>
2019-09-23 13:21:13 -0700

    finished privateRouting and Login Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import RegisterForm from './components/RegisterForm';
 import CreateItem from './components/CreateItem';
 import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-      <RegisterForm />
-      <CreateItem />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' render={props => <LoginForm {...props}/>} />
+        <RegisterForm />
+        <CreateItem />
+        <PrivateRoute path='home/profilepage' component={'home/profilepage'} />
+      </div>
+    </Router>
+
   );
 }
 
