f21a269bfc4aef854c830f0d9ff38d5a1dfae144
ericbanker12 <ericbanker12@gmail.com>
2019-09-23 15:07:01 -0500

    add styling to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Container, Row } from 'reactstrap'
+import ItemList from './ItemList';
 
 function Home(props) {
+    const [userLists, setUserLists] = useState([])
+
     return (
-        <div>Hello World</div>
+        <section>
+            <h2>My Lists</h2>
+            <Container>
+                <Row>
+                    <div>
+                        {/* {userLists.map(list=><ItemList {...list} />)} */}
+                    </div>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
