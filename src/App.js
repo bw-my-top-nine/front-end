e8fc92edbd4c47272ba2a45332ba35e0a2d750a4
Eric Richards <esholundr@yahoo.com>
2019-09-24 13:40:21 -0600

    few changes to CreateCategoryForm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
 
-import Header from './components/Header'
-import RegisterForm from './components/RegisterForm';
-import CreateItem from './components/CreateItem';
-import LoginForm from './components/LoginForm';
-import PrivateRoute from './components/PrivateRoute';
-import ItemList from './components/ItemList';
+import Header from "./components/Header";
+import RegisterForm from "./components/RegisterForm";
+import CreateItem from "./components/CreateItem";
+import LoginForm from "./components/LoginForm";
+import PrivateRoute from "./components/PrivateRoute";
+import ItemList from "./components/ItemList";
 
 function App() {
-  return (
-      <div className="App">
-        {/*<Header />*/}
-        <Route exact path='/' render={props => <LoginForm {...props}/>} />
-        <Route path ='/register-form' component={RegisterForm} />
-        {/*<CreateItem />*/}
-        <PrivateRoute path='home/profilepage' component={'home/profilepage'} />
-        {/*<ItemList />*/}
-      </div>
-
-  );
+	return (
+		<div className="App">
+			{/*<Header />*/}
+			<Route exact path="/" render={props => <LoginForm {...props} />} />
+			<Route path="/register-form" component={RegisterForm} />
+			{/*<CreateItem />*/}
+			<PrivateRoute path="home/profilepage" component={"home/profilepage"} />
+			{/*<ItemList />*/}
+		</div>
+	);
 }
 
 export default App;
