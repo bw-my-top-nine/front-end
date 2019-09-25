6f8ea655f75bef866b9d3aac4688ecdd424d90bd
ian-sanchez <silkyhairbear@gmail.com>
2019-09-25 13:58:22 -0700

    organizing thoughts

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,9 +9,9 @@ import CreateCategoryForm from './CreateCategoryForm';
 function Home(props) {
 //no props in home    
     console.log('props in Home', props);
+    const userId = localStorage.getItem('userId')
 //for the getData kind of useless rn
     const [edit, setEdit] = useState()
-    const [userId, setUserId] = useState(localStorage.getItem('userId'))
     const [userLists, setUserLists] = useState([])
 //Axios call to get UserList, kind of useless a.t.m
 //could use it to rerender something after updating serverdata? 
@@ -25,6 +25,8 @@ function Home(props) {
 //    }
 
     // Not sure if called here or parent...
+
+//1. axios call to get categories    
     useEffect(()=>{
         axios.get(`https://top-nine.herokuapp.com/api/categories/${userId}/categories`)
             .then(resp=>{
@@ -35,6 +37,48 @@ function Home(props) {
                 console.error(err)
             })
     },[])
+
+//2. post request to post categories
+    const createCategory = () => {
+        axios.post('https://top-nine.herokuapp.com/api/categories/:id/categories')
+        .then(resp => {
+            console.log(resp)
+            props.history.push('/home')
+        })
+    }
+//3. axios call to get items
+    const getItems = () => {
+        axios.get('https://top-nine.herokuapp.com/api/items')
+        .then(resp => {
+            console.log(resp)
+            setItemlist(resp.data)
+        })
+    }
+
+
+//4. post request to post items
+const addItem = () => {
+    //takes in itemList
+    axios.post('https://top-nine.herokuapp.com/api/items/:id/items')
+    .then(resp => {
+        props.history.push('')
+    })
+}
+//5. put request to edit category
+const saveEdit = (e) => {
+    //takes in updatedcategory
+    axios.put('https://top-nine.herokuapp.com/api/items/:id', newitemlist)
+    .then(res => {
+        console.log(res)
+        props.history.push('/home')
+    })
+    
+}
+//7. deleting category 
+//6. editing items
+    const editItem = () => {
+        axios.put
+    }
 
     return (
         <section>
