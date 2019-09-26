167ea509dfbfb3f7e9162dc47a0a9a090fc6cc34
ericbanker12 <ericbanker12@gmail.com>
2019-09-26 16:55:42 -0500

    prevent search submit

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Search = (props) => {
     return(
         <div>
-            <form>
+            <form onSubmit={e=>{e.preventDefault()}}>
                 <input 
                 type='text'
                 name='name'
