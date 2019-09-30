37ae3b775be18fcaa568d32396f789fd748bed11
ericbanker12 <ericbanker12@gmail.com>
2019-09-30 11:28:09 -0500

    remove category padding and stretching, add list top/bottom margin

--- src/components/MyLists.js
+++ src/components/MyLists.js
@@ -42,7 +42,7 @@ function MyLists(props) {
             <Row className="justify-content-center my-3">
                 <Search setSearch={setSearch} categories={categories} updateCategory={setCategories} />
             </Row>
-            <Row>
+            <Row className="align-items-start">
                 {
                     categories.map(category => {
                         return (
